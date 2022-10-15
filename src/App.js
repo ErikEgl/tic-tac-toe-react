fac390ae0fd9fe246b7cfbde35fb8cc99456d681
Erik <Mr.erik.egliens@gmail.com>
2022-10-15 22:58:13 +0300

    added react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
 import GamePage from "./components/GamePage/GamePage";
+import Scoreboard from "./components/Scoreboard/Scoreboard";
+import { Link, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <InitialPage/>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/tic-tac-toe-react/game-board">Game board</Link>
+          </li>
+          <li>
+            <Link to="/tic-tac-toe-react/scoreboard">Scoreboard</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route exact path="/tic-tac-toe-react/game-board" element={<GamePage />} />
+        <Route exact path="/tic-tac-toe-react/game-board" element={<Scoreboard />} />
+      </Routes>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import { AppContextProvider } from "./utils/useContext";
-import "./index.css"
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
 
 ReactDOM.render(
-<AppContextProvider>
-<App />
-</AppContextProvider>, document.getElementById("root"));
+  <AppContextProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </AppContextProvider>,
+  document.getElementById("root")
+);
