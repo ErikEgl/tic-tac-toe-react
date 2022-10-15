9879897867e877382d46d314bb56bb7c81d3b77c
Erik <Mr.erik.egliens@gmail.com>
2022-10-15 22:16:56 +0300

    initial page component

---
+++ src/components/InitialPage/InitialPage.js
@@ -0,0 +1,11 @@
+import Input from "../Input/Input";
+function InitialPage(props) {
+    return ( 
+        <>
+            <Input text="First Player"/>
+            <Input text="Second Player"/>
+        </>
+     );
+}
+
+export default InitialPage;
