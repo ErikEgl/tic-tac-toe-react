3d84126612ca95e6104ade4f37e26e3c6b5952cb
Erik <Mr.erik.egliens@gmail.com>
2022-10-18 00:54:00 +0300

    game page with styles

--- src/components/GamePage/GamePage.js
+++ src/components/GamePage/GamePage.js
@@ -1,12 +1,39 @@
 import Button from "../Button/Button";
 import Input from "../Input/Input";
 import Squares from "../Squares/Squares";
+import { useContext } from "react";
+import { UserContext } from "../../utils/useContext";
+
 function GamePage(props) {
-    return ( 
+  const { gameStarted, setGamePanel, winner, isX, startGame } = useContext(UserContext);
+  return (
+    <main>
+      {!gameStarted ? (
+        <>
+          <div className="input-container">
+            <Input text="First Player" />
+            <Input text="Second Player" />
+          </div>
+          <div>
+            <Button text="Start game" />
+          </div>
+        </>
+      ) : (
         <>
           <Squares />
+          <div>
+            <p>
+              {winner ? `Winner: ${winner}` : `Next Player: ${isX ? "✕" : "O"}`}
+            </p>
+
+            <button className="start-button" onClick={() => startGame()}>
+              Start Game
+            </button>
+          </div>
         </>
-     );
+      )}
+    </main>
+  );
 }
 
 export default GamePage;
