77d3bdd7d60f3437ffd31b1a63f345de46841072
Erik <Mr.erik.egliens@gmail.com>
2022-10-18 00:53:02 +0300

    added squares on page with styles

--- src/components/GamePage/GamePage.js
+++ src/components/GamePage/GamePage.js
@@ -1,11 +1,10 @@
 import Button from "../Button/Button";
 import Input from "../Input/Input";
+import Squares from "../Squares/Squares";
 function GamePage(props) {
     return ( 
         <>
-            <Input text="First Player"/>
-            <Input text="Second Player"/>
-            <Button text="Start game"/>
+          <Squares />
         </>
      );
 }
