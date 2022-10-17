11becaf7310738fa25b6afb799374392d2a89d14
Erik <Mr.erik.egliens@gmail.com>
2022-10-18 00:53:22 +0300

    input styles

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -3,11 +3,12 @@ import { UserContext } from "../../utils/useContext";
 
 
 function Input({text}) {
+
     const { handlePlayerData, playerData } = useContext(UserContext);
     return ( 
         <>
             <label>
-                {text.toUpperCase()}
+                {text}
                 <input onChange={handlePlayerData} type="text" name={text === "First Player" ? "firstPlayer" : "secondPlayer"} value={text === "First Player" ? playerData.firstPlayer : playerData.secondPlayer}/>
             </label>
         </>
