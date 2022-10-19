572ac246cfffab98e8db10d63735deb675146dea
Erik <Mr.erik.egliens@gmail.com>
2022-10-20 01:02:29 +0300

    disabled btn styles

--- src/components/TimeTravel/TimeTravel.js
+++ src/components/TimeTravel/TimeTravel.js
@@ -2,12 +2,12 @@ import { useContext } from "react";
 import { UserContext } from "../../utils/useContext";
 
 function TimeTravel() {
-  const { jumpInTime, gameHistory } = useContext(UserContext);
+  const { jumpInTime, gameHistory, winner, tie } = useContext(UserContext);
   const historyButtons = gameHistory.map((step, i) => {
     const destination = i ? `Go to move #${i}` : "Go to start";
     return (
       <li key={i}>
-        <button onClick={() => jumpInTime(i)}>{destination}</button>
+        <button disabled={winner || tie} onClick={() => jumpInTime(i)}>{destination}</button>
       </li>
     );
   });
